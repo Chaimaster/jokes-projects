6f608697984a30e71cc20c7fa5edc80fe718a90f
Chaimaster <dchai2014@gmail.com>
2015-08-28 15:26:38 +0000

    write a message

--- application_controller.rb
+++ application_controller.rb
@@ -1,9 +1,16 @@
 require 'bundler'
+require_relative 'models/model.rb'
 Bundler.require
 
 class MyApp < Sinatra::Base
 
   get '/' do
     erb :index
+  end
+  post '/' do
+    puts params
+    jk=Jokes.new
+    @joke=jk.get_joke(params[:jokes])
+    erb :respond
   end
 end
